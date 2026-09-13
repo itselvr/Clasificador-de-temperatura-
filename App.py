a52a3365934bb68e91a9afb99a07e210295f11a1
itsel <itselvr@gmail.com>
2026-09-12 21:18:27 -0600

    Fix variable name from 'temperature' to 'temperatura'

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("clasificador de temperatura")
 temperatura = st. number_input("Introduce la temperatura en °C:", value=20)
-if temperature< 10:
+if temperatura< 10:
   st.write("hace frio.")
 elif temperatura< 25:
   st.write("la temperatura es agradable.")

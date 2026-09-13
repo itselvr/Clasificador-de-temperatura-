e34eb056c991520063bf953dacf0574aec9fbc3b
itsel <itselvr@gmail.com>
2026-09-12 21:17:10 -0600

    Add temperature classifier using Streamlit

--- App.py
+++ App.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+st.title("clasificador de temperatura")
+temperatura = st. number_input("Introduce la temperatura en °C:", value=20)
+if temperature< 10:
+  st.write("hace frio.")
+elif temperatura< 25:
+  st.write("la temperatura es agradable.")
+else:
+  st.write("hace calor.")
